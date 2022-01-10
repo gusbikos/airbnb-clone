94d638a1389cec8c172ae8a6bc49b8dc6738d2e6
Gus Bikos <gusbikos@gmail.com>
2022-01-10 16:02:43 -0500

    first commit header styling done

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+import Home from './Home.js'
+import Header from './Header.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <Header />
+        <Home />
+
+          {/* Header */}
+
+          {/* Banner */}
+            {/* Search */}
+
+          {/* Cards */}
+
+          {/* Footer */}
+
+        {/* SearchPage */}
+      </div>
+    )
+  }
+}
+
+export default App;
