7513e4b8f589c45ace8cae09fedda60ae2ab6cfa
Gus Bikos <gusbikos@gmail.com>
2022-01-10 20:57:28 -0500

    started banner component and added styling

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Banner from './Banner'
 import './Home.css'
 
 const Home = () => {
     return (
         <div className='home'>
+            <h1>home component</h1>
+            <Banner />
         </div>
     )
 }
